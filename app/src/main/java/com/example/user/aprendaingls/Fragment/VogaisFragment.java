964baacd80e43322a7df5f60e5e614b41d84591b
rayller <rayller.emery@gmail.com>
2019-01-10 17:42:51 -0200

    commit

--- app/src/main/java/com/example/user/aprendaingls/Fragment/VogaisFragment.java
+++ app/src/main/java/com/example/user/aprendaingls/Fragment/VogaisFragment.java
@@ -1,19 +1,22 @@
 package com.example.user.aprendaingls.Fragment;
 
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.example.user.aprendaingls.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class VogaisFragment extends Fragment {
-
+public class VogaisFragment extends Fragment implements View.OnClickListener{
+    private MediaPlayer mediaPlayer;
+    private ImageButton a, e, i, o, u;
 
     public VogaisFragment() {
         // Required empty public constructor
@@ -24,7 +27,63 @@ public class VogaisFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_vogais, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_vogais, container, false);
+
+        a = view.findViewById(R.id.imgBtnA);
+        e = view.findViewById(R.id.imgBtnE);
+        i = view.findViewById(R.id.imgBtnI);
+        o = view.findViewById(R.id.imgBtnO);
+        u = view.findViewById(R.id.imgBtnU);
+
+        a.setOnClickListener(this);
+        e.setOnClickListener(this);
+        o.setOnClickListener(this);
+        i.setOnClickListener(this);
+        u.setOnClickListener(this);
+
+        return view;
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case (R.id.imgBtnA):
+                exeSound(mediaPlayer = MediaPlayer.create(getActivity(), R.raw.a));
+                break;
+            case(R.id.imgBtnE):
+                exeSound(mediaPlayer = MediaPlayer.create(getActivity(), R.raw.e));
+                break;
+            case(R.id.imgBtnI):
+                exeSound(mediaPlayer = MediaPlayer.create(getActivity(), R.raw.i));
+                break;
+            case(R.id.imgBtnO):
+                exeSound(mediaPlayer = MediaPlayer.create(getActivity(), R.raw.o));
+                break;
+            case(R.id.imgBtnU):
+                exeSound(mediaPlayer = MediaPlayer.create(getActivity(), R.raw.u));
+                break;
+        }
+
+    }
+
+    public void exeSound(final MediaPlayer mediaPlayer){
+        mediaPlayer.start();
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mediaPlayer.release();
+                mp.release();
+            }
+        });
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(mediaPlayer != null && !mediaPlayer.isPlaying()){
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
 }
